0318cb14fc8f21dbace81ff2cf2e39fa985abb9a
areifaasarudeen-ux <areifaasarudeen@gmail.com>
2025-11-30 15:13:20 +0300

    Added web configurer

--- src/main/java/com/raqaf/ecom/EcomApplication.java
+++ src/main/java/com/raqaf/ecom/EcomApplication.java
@@ -2,6 +2,9 @@ package com.raqaf.ecom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(scanBasePackages="com.raqaf.ecom")
 public class EcomApplication {
@@ -9,5 +12,17 @@ public class EcomApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EcomApplication.class, args);
 	}
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer(){
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("*");
+            }
+        };
+    }
 
 }
